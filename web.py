ddd92f6ce672e3c2f864e2381647602ca9666b7c
pri-kleine <63248623+pri-kleine@users.noreply.github.com>
2022-09-29 18:38:57 -0300

    alerta

--- web.py
+++ web.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Priscilla K"
+    return "Paiva ACORDA!!!"
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
